0f000877ca3c8b3ec1d7ef4eee160f18dd4a7ffa
Francesco de Guytenaere <guytenaere.fe@gmail.com>
2020-01-04 13:24:24 +0100

    updated stocksChanged function code

--- start/stocksChanged/index.js
+++ start/stocksChanged/index.js
@@ -1,5 +1,9 @@
-module.exports = async function (context, documents) {
-    if (!!documents && documents.length > 0) {
-        context.log('Document Id: ', documents[0].id);
-    }
+export default async function (context, documents) {
+    const updates = documents.map(stock => ({
+        target: 'updated',
+        arguments: [stock]
+    }));
+
+    context.bindings.signalRMessages = updates;
+    context.done();
 }
